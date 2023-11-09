c2f2404dedfcc005f7e4ab2726b7c9144d59c049
mayur <mayurtitirmare261@gmail.com>
2023-11-09 13:15:32 +0530

    customer user login in jsp page redirect

---
+++ src/main/java/in/com/springsecurity/SpringSecurityUsernamePasswordUserDefineApplication.java
@@ -0,0 +1,13 @@
+package in.com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUsernamePasswordUserDefineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUsernamePasswordUserDefineApplication.class, args);
+	}
+
+}
